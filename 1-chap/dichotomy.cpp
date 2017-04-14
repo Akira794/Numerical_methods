ef57278582258fb0b8555ae301a52ef983b9b820
Akira794 <s1426079kv@s.chibakoudai.jp>
2017-04-14 17:49:46 +0900

    add dichotomy

---
+++ 1-chap/dichotomy.cpp
@@ -0,0 +1,47 @@
+/* 2分法　*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+#define EPS .0001
+
+double dichotomy(double, double);
+double func_y(double);
+
+int main(int argc, char* argv[])
+{
+	double a = 0.0, b = 1.0; //Initial value setting
+	double x; //Solution
+	
+	cout << "x^3 + 5x - 4 = 0 の2分法による数値計算" << endl;
+	
+	cout << "初期値 a = " << a << endl;
+	cout << "初期値 b = " << b << endl;
+	
+	x = dichotomy(a, b);
+	cout << "近似解 x = " << x << endl;
+
+	return 0;
+}
+
+double dichotomy(double a, double b)
+{
+	double c;
+	
+	do{
+		c = (a + b) / 2.0; /* 2分計算 */
+		
+		if( (func_y(c) * func_y(a) ) < 0 ) b = c;
+
+		else a = c;
+
+	}while ( fabs(a - b) > EPS );
+
+	return c;
+}
+
+double func_y(double x)
+{
+	return pow(x, 3.0) + (5.0 * x) - 4.0;
+}
